cf52375671eea31ccd1b7221040fdfe28a06d254
bbazang <bbazang@gmail.com>
2022-12-26 14:21:21 +0900

    c

--- 소스.cpp
+++ 소스.cpp
@@ -6,21 +6,9 @@ using namespace std;
 
 int main()
 {
-	int a = 0;
-	int b = 0;
-	int c = 0;
-
-	float x1 = 0;
-	float x2 = 0;
-
-	cin >> a;
-	cin >> b;
-	cin >> c;
-
-	x1 = (- b + sqrt(b * b - 4 * a * c)) / (2 * a);
-	x2 = (- b - sqrt(b * b - 4 * a * c)) / (2 * a);
-	cout << x1 << endl;
-	cout << x2 << endl;
+	char C = 'A';
+	cout << C << endl;
 	
 	return 0;
+
 }
